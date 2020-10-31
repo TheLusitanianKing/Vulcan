f6b8acca9ce2fcd5b5b41b3d6aab2b68e476c5a1
AlexLusitania <alex.lisboa.metalhead@gmail.com>
2020-10-31 22:33:24 +0000

    master : more generic examples

--- MergeRequest.hs
+++ MergeRequest.hs
@@ -10,12 +10,12 @@ data MergeRequest = MergeRequest {
 } deriving (Show)
 
 -- merge request typical examples :
--- https://git.kwankodev.net/kwanko/kwanko-members/merge_requests/2939/diffs
-mr1 = MergeRequest 2939 "https://git.kwankodev.net/" "kwanko/kwanko-members"
--- https://git.kwankodev.net/kwanko/kwanko-api-backend/merge_requests/1993/diffs
-mr2 = MergeRequest 1993 "https://git.kwankodev.net/" "kwanko/kwanko-api-backend"
--- https://git.kwankodev.net/addons/api-fronto/merge_requests/1580
-mr3 = MergeRequest 1580 "https://git.kwankodev.net/" "addons/api-fronto"
+-- https://git.something.com/project/front/merge_requests/2939/diffs
+mr1 = MergeRequest 2939 "https://git.something.com/" "project/front"
+-- https://git.something.com/project/api/merge_requests/1993/diffs
+mr2 = MergeRequest 1993 "https://git.something.com/" "project/api"
+-- https://git.something.com/project/api-front/merge_requests/1580
+mr3 = MergeRequest 1580 "https://git.something.com/" "project/api-front"
 
 -- parse a merge request URI
 parseMergeRequest :: String -> Maybe MergeRequest
