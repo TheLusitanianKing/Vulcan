77c9791d5e549fa44a38ee9af375a199a0fe9265
AlexLusitania <alex.lisboa.metalhead@gmail.com>
2020-11-09 19:33:45 +0000

    master : cleaning cabal file w/ setup and new main action

--- Main.hs
+++ Main.hs
@@ -1,9 +1,13 @@
 module Main where
 
-import System.Environment (getArgs)
+import MergeRequest (parseMergeRequest, printMergeRequest)
 
 main :: IO ()
 main = interact $ action
     
 action :: String -> String
-action = undefined
+action m =
+    case parsed of
+        Nothing -> "This isn't a merge request!"
+        Just x  -> "This is a valid merge request: " ++ printMergeRequest x
+    where parsed = parseMergeRequest m
