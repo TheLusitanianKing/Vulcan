1a33675a9d943a22e447465facbf1901d56c13d9
AlexLusitania <alex.lisboa.metalhead@gmail.com>
2020-11-19 22:08:37 +0000

    master : configuration not yet used

--- src/Submodule.hs
+++ src/Submodule.hs
@@ -1,16 +1,25 @@
 module Submodule where
 
-import Data.ConfigFile (emptyCP, readfile, to_string)
-import Data.Either.Utils (forceEither)
-
 data Submodule = Submodule {
     submoduleId   :: Int,
     submoduleName :: String
 } deriving (Eq, Show)
 
--- retrieveSubmodulesFromConfigurationFile :: FilePath -> IO (Maybe [Submodule])
-retrieveSubmodulesFromConfigurationFile :: FilePath -> IO ()
-retrieveSubmodulesFromConfigurationFile m = do
-    val <- readfile emptyCP m
-    putStrLn "Your setting is:"
-    putStrLn . to_string . forceEither $ val
+
+fromRaw :: (String, Int) -> Submodule
+fromRaw (s, i) = Submodule i s
+
+-- TODO: get this from configuration, not from here
+submodulesRaw :: [(String, Int)]
+submodulesRaw = [
+        ("something", 39),
+        ("something/else", 72)
+    ]
+
+submodules :: [Submodule]
+submodules = map fromRaw submodulesRaw
+
+    
+-- TODO: improve this
+retrieveSubmodulesFromConfiguration :: FilePath -> IO (Maybe [Submodule])
+retrieveSubmodulesFromConfiguration = undefined
