88984cd51b7189d4dd18ee28a14f144df5e1a739
AlexLusitania <alex.lisboa.metalhead@gmail.com>
2020-11-19 18:55:24 +0000

    master : add function to retrieve submodules from configuration file

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,7 @@
 module Main where
 
 import System.Environment (getArgs)
-import MergeRequest
+import MergeRequest (parseMergeRequest, printMergeRequest)
 
 main :: IO ()
 main = getArgs >>=
--- src/Submodule.hs
+++ src/Submodule.hs
@@ -1,6 +1,16 @@
 module Submodule where
 
+import Data.ConfigFile (emptyCP, readfile, to_string)
+import Data.Either.Utils (forceEither)
+
 data Submodule = Submodule {
-    submoduleId   :: Int
+    submoduleId   :: Int,
     submoduleName :: String
 } deriving (Eq, Show)
+
+-- retrieveSubmodulesFromConfigurationFile :: FilePath -> IO (Maybe [Submodule])
+retrieveSubmodulesFromConfigurationFile :: FilePath -> IO ()
+retrieveSubmodulesFromConfigurationFile m = do
+    val <- readfile emptyCP m
+    putStrLn "Your setting is:"
+    putStrLn . to_string . forceEither $ val
