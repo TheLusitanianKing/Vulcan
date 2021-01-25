d6cc4129886c429477b0707759deca91b7184806
TheLusitanianKing <alex.lisboa.metalhead@gmail.com>
2021-01-25 23:13:06 +0000

    adding unit tests on merge request URL testing

---
+++ tests/Tests.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Test.Hspec
+import Test.QuickCheck
+import Control.Exception (evaluate)
+import MergeRequest
+
+main :: IO ()
+main = hspec $ do
+  describe "Testing merge request URL parsing..." $ do
+    it "Parsing correct merge request URL" $ do
+      parseMergeRequest "https://git.test.com/project/api-backend/merge_requests/2101"
+      `shouldBe`
+      Just (MergeRequestURL {
+        mergeRequestId = 2101,
+        mergeRequestProject = "api-backend",
+        mergeRequestBaseURL = "https://git.test.com"
+      })
+
+    it "Parsing correct merge request URL (2)" $ do
+      parseMergeRequest "https://gitlab.com/TheLusitanianKing/Lusitania/-/merge_requests/1"
+      `shouldBe`
+      Just (MergeRequestURL {
+        mergeRequestId = 1,
+        mergeRequestProject = "Lusitania",
+        mergeRequestBaseURL = "https://gitlab.com"
+      })
+
+    it "Parsing merge request URL with wrong number" $ do
+      parseMergeRequest "https://gitlab.com/TheLusitanianKing/project/-/merge_requests/ekgkle"
+      `shouldBe`
+      Nothing
+
+    it "Parsing malformed merge request URL" $ do
+      parseMergeRequest "https://gitlab.com/merge_requests/ekgkle"
+      `shouldBe`
+      Nothing
+
+    it "Parsing malformed merge request URL (2)" $ do
+      parseMergeRequest "https://gitlab.com/TheLusitanianKing/merge_requests/ekgkle"
+      `shouldBe`
+      Nothing
+
+    it "Parsing malformed merge request URL (3)" $ do
+      parseMergeRequest "https://gitlab.com/TheLusitanianKing////merge_requests/ekgkle"
+      `shouldBe`
+      Nothing
